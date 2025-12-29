01a24bb385fa6c94adbbe9a7d45df5ced2583200
atulkumar51302 <atulkumar51302@gmail.com>
2025-12-29 22:44:14 +0530

    Initialize main.py with project details
    day 1

---
+++ main.py
@@ -0,0 +1,6 @@
+# My Subscription manager -Day 1
+#today i learn only variable and print statement 
+project_name = "Smart Subscription Manager"
+goal = "to track my expenses"
+print ("project Name:",project_name)
+print("Project Goal:", goal)
