4e901f32e89c5dfe621fe326fc231b1ff7cca2c4
atulkumar51302 <atulkumar51302@gmail.com>
2026-01-07 23:34:13 +0530

    Add initial implementation in unknown.py

---
+++ unknown.py
@@ -0,0 +1,10 @@
+name = "atul kumar"
+list = [2,22,44,53,11]
+counter=0
+# for char in name:
+for number in list:
+    # print(counter,name)
+    print(counter,number)
+    counter+=1
+#slicing    
+# print(name[:3])    
